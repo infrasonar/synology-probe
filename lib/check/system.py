2b14e79b2d4881fd84a3564fb048e69a10a6d0db
Koos85 <koosjoosten@gmail.com>
2023-01-28 11:28:55 +0100

    system check one type

--- lib/check/system.py
+++ lib/check/system.py
@@ -1,5 +1,6 @@
 from asyncsnmplib.mib.mib_index import MIB_INDEX
 from libprobe.asset import Asset
+from libprobe.exceptions import CheckException
 from ..utils import get_data
 
 QUERIES = (
@@ -8,6 +9,19 @@ QUERIES = (
     MIB_INDEX['SYNOLOGY-SYSTEM-MIB']['fan'],
 )
 
+STATUS = {
+    1: 'Normal',
+    2: 'Failed',
+}
+
+UPGRADE = {
+    1: 'Available',
+    2: 'Unavailable',
+    3: 'Connecting',
+    4: 'Disconnected',
+    5: 'Others',
+}
+
 
 async def check_system(
         asset: Asset,
@@ -15,4 +29,25 @@ async def check_system(
         check_config: dict) -> dict:
 
     state = await get_data(asset, asset_config, check_config, QUERIES)
-    return state
+    try:
+        system = state['synoSystem'][0]
+        fan = state['fan'][0]
+        dsm_info = state['dsmInfo'][0]
+    except Exception:
+        raise CheckException('incomplete data')
+
+    return {
+        'system': {
+            'name': 'system',
+            'systemStatus': STATUS.get(system.get('systemStatus')),
+            'temperature': system.get('temperature'),
+            'powerStatus': STATUS.get(system.get('powerStatus')),
+            'controllerNumber': system.get('controllerNumber'),
+            'systemFanStatus': STATUS.get(fan.get('systemFanStatus')),
+            'cpuFanStatus': STATUS.get(fan.get('cpuFanStatus')),
+            'modelName': dsm_info.get('modelName'),
+            'serialNumber': dsm_info.get('serialNumber'),
+            'version': dsm_info.get('version'),
+            'upgradeAvailable': UPGRADE.get(dsm_info.get('upgradeAvailable')),
+        }
+    }
