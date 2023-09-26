ed65811ee6ab0545cf4abd3b07fec940981cb0d6
Koos85 <koosjoosten@gmail.com>
2023-09-26 15:07:32 +0200

    exceptions

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,5 @@
-from asyncsnmplib.exceptions import SnmpNoConnection, SnmpNoAuthParams
-from asyncsnmplib.utils import InvalidConfigException, snmp_queries
+from asyncsnmplib.utils import snmp_queries
 from libprobe.asset import Asset
-from libprobe.exceptions import CheckException, IgnoreResultException
 
 
 async def get_data(
@@ -14,10 +12,6 @@ async def get_data(
         address = asset.name
     try:
         state = await snmp_queries(address, asset_config, queries)
-    except SnmpNoConnection:
-        raise CheckException('unable to connect')
-    except (InvalidConfigException, SnmpNoAuthParams):
-        raise IgnoreResultException
     except Exception:
         raise
     else:
