f601e2f9fb95b8c5343b63096daa616ece64f511
Jeroen van der Heijden <jeroen@cesbit.com>
2023-09-28 13:01:13 +0200

    upd req and simplify

--- lib/utils.py
+++ lib/utils.py
@@ -10,9 +10,6 @@ async def get_data(
     address = check_config.get('address')
     if address is None:
         address = asset.name
-    try:
-        state = await snmp_queries(address, asset_config, queries)
-    except Exception:
-        raise
-    else:
-        return state
+
+    state = await snmp_queries(address, asset_config, queries)
+    return state
